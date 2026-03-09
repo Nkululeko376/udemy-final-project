1dd3a446b7825d4d545109a154abfcfff23e7cd8
Nkululeko Fakude <mtolofakude96@gmail.com>
2026-03-09 21:22:12 +0200

    start making the website responsive by making the navigation responsive.

---
+++ index.js
@@ -0,0 +1,9 @@
+// let menuBtn = document.querySelector("#menuBtn");
+// let webBody = document.querySelector("body");
+
+// menuBtn.addEventListener("click", () => {
+//   // alert("The button was clicked");
+//   // webBody.style = "background-color: red;";
+
+//   webBody.classList.add("my-body");
+// });
